518478377390cdb7b39dbf336c3ef47b37232162
raamir <raamir@mail.depaul.edu>
2019-02-13 22:41:20 -0500

    Tutorial Part 3

---
+++ src/components/layout.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+export default ({ children }) => (
+  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/index-layout" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/index-layout">Home</ListLink>
+        <ListLink to="/about-layout/">About</ListLink>
+        <ListLink to="/contact-layout/">Contact</ListLink>
+      </ul>
+    </header>
+    {children}
+  </div>
+)
